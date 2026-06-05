8906f1b898e69ba4cdcf600c818ddabf019145d8
elijcastro <129324768+elijcastro@users.noreply.github.com>
2026-06-05 00:02:10 -0600

    Avance del día

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Compilando Nightfall_3D...
 
-"C:\msys64\mingw64\bin\g++.exe" src/*.cpp src/glad.c -Iinclude -LC:/msys64/mingw64/lib -lglfw3 -lopengl32 -lgdi32 -o app.exe
+c:\Laboratorios_OPENGL\Nightfall_3D\build\app.exe
 
 echo.
 echo Compilación finalizada.
